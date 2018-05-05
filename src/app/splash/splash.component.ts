ae41699a7d5d05dcb320f0b82ce80b07d06c8a81
James Ticer <jticer4@icloud.com>
2018-05-04 20:50:28 -0600

    added my users into the splash.component.ts file

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,9 +1,18 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {User} from "../shared/classes/user";
+import {UserService} from "../shared/services/user.service";
 
 @Component({
 	template: require("./splash.component.html")
 })
 
-export class SplashComponent {
-	
+export class SplashComponent implements OnInit{
+	users: User[] = [];
+
+	constructor(protected userService: UserService) {}
+
+	ngOnInit():void{
+		this.userService.getAllUsers()
+			.subscribe(users => this.users = users);
+	}
 }
